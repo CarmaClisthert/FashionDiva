e4424b4f940e9c87b18cade2f37b50ae35ab7d47
Mariam Sanou <79387601+mariamsanou@users.noreply.github.com>
2025-04-22 13:42:44 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -6,3 +6,15 @@ CREATE TABLE users (
     password TEXT NOT NULL
 );
 
+CREATE TABLE clothing_items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    image_path TEXT NOT NULL,
+    --item_type TEXT,
+    --attributes TEXT,
+    --embedding BLOB,
+    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+

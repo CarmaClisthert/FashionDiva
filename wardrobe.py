3669fa3b6f84d3e3e5dc834f81bbc8c7056011d3
Umorilol <99937650+Umorilol@users.noreply.github.com>
2025-05-01 14:03:04 -0400

    Tweaks

--- wardrobe.py
+++ wardrobe.py
@@ -1,31 +1,52 @@
 import os 
-from flask import Blueprint, request, redirect, session, url_for, render_template
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 from werkzeug.utils import secure_filename
 from database.db import get_db
 
-bp = Blueprint('wardrobe', __name__)
+bp = Blueprint('wardrobe', __name__, url_prefix='/wardrobe')
 
-@bp.route('/upload', methods=['POST'])
-def upload():
-    image = request.file.get('image')
-    if not image:
-        return redirect(url_for('wardrobe.view_wardrobe'))
-    
-    filename = secure_filename(image.filename)
-    upload_path = os.path.join('app', 'static', 'uploads', filename)
-    image.save(upload_path)
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
-    # add mmfashion attribute and landmark recognition here
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    db = get_db()
-    db.execute('INSERT INTO clothing_items (user_id, filename VALUES (?, ?,)',
-               (session['user_id'], filename))
-    db.commit()
-
-    return redirect(url_for('wardrobe.view_wardrobe'))
-
-@bp.route('/wardrobe')
-def veiw_wardrobe():
+@bp.route('/', methods=['GET', 'POST'])
+def view_wardrobe():
     db = get_db()
     items = db.execute('SELECT * FROM clothing_items WHERE user_id = ?', (session['user_id'],)).fetchall()
     return render_template('upload_closet.html', items=items)
+
+@bp.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+
+            db = get_db()
+            db.execute(
+                'INSERT INTO clothing_items (user_id, image_path) VALUES(?, ?)',
+                (session['user_id'], filename),
+            )
+            db.commit()
+            return redirect(url_for('wardrobe.view_wardrobe'))
+    return render_template("upload_closet.html")
+
+@bp.route('/debug')
+def debug_db():
+    db = get_db()
+    items = db.execute('SELECT * FROM clothing_items').fetchall()
+    for item in items:
+        print(dict(item))  # convert Row object to dictionary
+    return "Check console for output"
